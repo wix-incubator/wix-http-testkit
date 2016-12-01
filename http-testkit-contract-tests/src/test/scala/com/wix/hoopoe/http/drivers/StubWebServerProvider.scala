4eb680a53508f9b6cd3fcffa97b2a87abd6eafd1
Noam Almog <noama@wix.com>
2016-12-01 16:34:25 +0200

    dead code

--- http-testkit-contract-tests/src/test/scala/com/wix/hoopoe/http/drivers/StubWebServerProvider.scala
+++ http-testkit-contract-tests/src/test/scala/com/wix/hoopoe/http/drivers/StubWebServerProvider.scala
@@ -1,10 +1,7 @@
 package com.wix.hoopoe.http.drivers
 
-import akka.http.scaladsl.model.HttpRequest
 import com.wix.hoopoe.http.BaseUri
-import com.wix.hoopoe.http.api.StubWebServer
 import com.wix.hoopoe.http.server.WebServerFactory.aStubWebServer
-import org.specs2.matcher.Matcher
 import org.specs2.mutable.After
 
 trait StubWebServerProvider extends After {
@@ -20,21 +17,21 @@ trait StubWebServerProvider extends After {
 
 
 
-object StubWebServerMatchers {
-  import org.specs2.matcher.Matchers._
-
-  def httpRequestWith(method: String, toPath: String): Matcher[HttpRequest] =
-    be_===( toPath ) ^^ { (_: HttpRequest).uri.path.toString().stripPrefix("/") aka "request path"} and
-      be_==[String](method).ignoreCase ^^ { (_: HttpRequest).method.name /*aka "method"*/ }
-
-  def httpRequestWith(header: (String, String)): Matcher[HttpRequest] =
-    havePair( header ) ^^ { (_: HttpRequest).headers.map( h => h.name -> h.value) aka "request headers" }
-
-  def receivedRequestWith(method: String, toPath: String): Matcher[StubWebServer] = {
-    contain(httpRequestWith(method, toPath)).eventually ^^ { (_: StubWebServer).recordedRequests aka "requests" }
-  }
-
-  def receivedRequestWith(header: (String, String)): Matcher[StubWebServer] = {
-    contain(httpRequestWith(header)).eventually ^^ { (_: StubWebServer).recordedRequests aka "requests" }
-  }
-}
+//object StubWebServerMatchers {
+//  import org.specs2.matcher.Matchers._
+//
+//  def httpRequestWith(method: String, toPath: String): Matcher[HttpRequest] =
+//    be_===( toPath ) ^^ { (_: HttpRequest).uri.path.toString().stripPrefix("/") aka "request path"} and
+//      be_==[String](method).ignoreCase ^^ { (_: HttpRequest).method.name /*aka "method"*/ }
+//
+//  def httpRequestWith(header: (String, String)): Matcher[HttpRequest] =
+//    havePair( header ) ^^ { (_: HttpRequest).headers.map( h => h.name -> h.value) aka "request headers" }
+//
+//  def receivedRequestWith(method: String, toPath: String): Matcher[StubWebServer] = {
+//    contain(httpRequestWith(method, toPath)).eventually ^^ { (_: StubWebServer).recordedRequests aka "requests" }
+//  }
+//
+//  def receivedRequestWith(header: (String, String)): Matcher[StubWebServer] = {
+//    contain(httpRequestWith(header)).eventually ^^ { (_: StubWebServer).recordedRequests aka "requests" }
+//  }
+//}
