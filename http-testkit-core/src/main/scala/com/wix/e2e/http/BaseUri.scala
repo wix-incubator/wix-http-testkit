22a21a6742cca67653e8f33c51b13afb22065bd3
Noam Almog <noama@wix.com>
2017-11-02 10:32:48 +0200

    fix: added better warning when client http testkit is missing base uri

--- http-testkit-core/src/main/scala/com/wix/e2e/http/BaseUri.scala
+++ http-testkit-core/src/main/scala/com/wix/e2e/http/BaseUri.scala
@@ -1,3 +1,8 @@
 package com.wix.e2e.http
 
+import scala.annotation.implicitNotFound
+
+@implicitNotFound(
+  """Cannot find an implicit BaseUri.
+     Please specify implicit val baseUri: BaseUri parameter.""")
 case class BaseUri(host: String = "localhost", port: Int, contextRoot: Option[String] = None)
