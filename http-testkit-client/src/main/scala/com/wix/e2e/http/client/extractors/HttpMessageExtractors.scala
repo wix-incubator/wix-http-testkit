1e1122b68485810d9ee3e580d05fbc2e4d91b378
Noam Almog <noama@wix.com>
2017-12-03 11:26:31 +0200

    client: refactored extractor code client: fixed a bug where extractors were not found on call site

--- http-testkit-client/src/main/scala/com/wix/e2e/http/client/extractors/HttpMessageExtractors.scala
+++ http-testkit-client/src/main/scala/com/wix/e2e/http/client/extractors/HttpMessageExtractors.scala
@@ -1,9 +1,28 @@
 package com.wix.e2e.http.client.extractors
 
-import com.wix.e2e.http.client.extractors.internals.{HttpEntityTransformers, HttpRequestExtractors, HttpResponseExtractors}
+import akka.http.scaladsl.model.{HttpEntity, HttpMessage}
+import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+import com.wix.e2e.http.WixHttpTestkitResources
+import com.wix.e2e.http.api.Marshaller
+import com.wix.e2e.http.utils.waitFor
 
-trait HttpMessageExtractors extends HttpEntityTransformers
-                               with HttpResponseExtractors
-                               with HttpRequestExtractors
+import scala.concurrent.duration._
+
+trait HttpMessageExtractors {
+  implicit class HttpMessageExtractorsOps[T <: HttpMessage](message: T) {
+    def extractAs[T : Manifest](implicit marshaller: Marshaller, atMost: FiniteDuration = 5.seconds): T = message.entity.extractAs[T]
+    def extractAsString(implicit atMost: FiniteDuration = 5.seconds): String = message.entity.extractAsString
+    def extractAsBytes(implicit atMost: FiniteDuration = 5.seconds): Array[Byte] = message.entity.extractAsBytes
+  }
+
+  implicit class HttpEntityExtractorsOps[E <: HttpEntity](entity: E) {
+    def extractAs[T : Manifest](implicit marshaller: Marshaller, atMost: FiniteDuration = 5.seconds): T = marshaller.unmarshall[T](extract[String])
+    def extractAsString(implicit atMost: FiniteDuration = 5.seconds): String = extract[String]
+    def extractAsBytes(implicit atMost: FiniteDuration = 5.seconds): Array[Byte] = extract[Array[Byte]]
+
+    import WixHttpTestkitResources.materializer
+    private def extract[T](implicit um: Unmarshaller[E, T], atMost: FiniteDuration = 5.seconds) = waitFor(Unmarshal(entity).to[T])(atMost)
+  }
+}
 
 object HttpMessageExtractors extends HttpMessageExtractors
