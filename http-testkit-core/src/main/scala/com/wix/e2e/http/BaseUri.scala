3f4ed93a1efafad40d2a4820d6d1cea16b5be605
Noam Almog <noama@wix.com>
2018-07-01 15:41:42 +0300

    client: updated missing base uri error message

--- http-testkit-core/src/main/scala/com/wix/e2e/http/BaseUri.scala
+++ http-testkit-core/src/main/scala/com/wix/e2e/http/BaseUri.scala
@@ -3,6 +3,6 @@ package com.wix.e2e.http
 import scala.annotation.implicitNotFound
 
 @implicitNotFound(
-  """Cannot find an implicit BaseUri.
+  """Cannot find system under test host/port.
      Please specify implicit val baseUri: BaseUri parameter.""")
 case class BaseUri(host: String = "localhost", port: Int, contextRoot: Option[String] = None)
